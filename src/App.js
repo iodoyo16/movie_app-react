feff0e6bba7dddc67ce59418b7bcf17d7f639197
iodoyo16 <durfbgksmsskfro@gmail.com>
2021-08-27 19:27:48 +0900

    function component, dealing props as parameter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+// component is function that returns html
+function Food({fav, picture, rating}){ //property object를 열어 fav 를 get
+  return <div>
+    <h1>I like {fav}</h1>
+    <img src={picture} alt={fav}/>
+    <h1>rating : {rating} </h1>
+  </div>
+}
+
+Food.propTypes={  // property 의 type check를 위한 description
+  fav: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired, // isRequired 필수
+  rating: PropTypes.number
+}
+function renderFood(food){
+  return <Food key={food.id} fav={food.name} picture={food.image} rating={food.rating} />;
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {foodILike.map(renderFood)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// virtual document object model
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//  import된 App component(<App .>), id='root' 인 html 요소
+// this combination between javascript and html called 'jsx'
